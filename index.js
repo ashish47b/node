20cdd009a887da3a28ba1f00d00aa6d435eb972b
Ashish Ranjan <ashishranjan47b@gmail.com>
2024-02-14 22:25:21 +0530

    verision update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const connectDb = require('./config/dbconnection');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 connectDb();
 app.use('/api/',userRoute)
 app.use(errorHandler); /// Not Remember
