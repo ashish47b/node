c58d101e7fcb5e7b0db5fefe1f89e4aa570bbf6a
Ashish Ranjan <ashishranjan47b@gmail.com>
2024-02-14 22:06:59 +0530

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute  = require('./routes/userRoute');
 const errorHandler  = require('./middleware/errorHandler'); 
 const connectDb = require('./config/dbconnection');
 const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors()); 
 app.use(express.json()); 
 connectDb();
 app.use('/api/',userRoute)
